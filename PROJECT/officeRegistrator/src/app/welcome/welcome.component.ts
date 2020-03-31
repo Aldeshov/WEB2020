d04281ca691ebaedda8c0a696cb6994415a3a33a
Aldeshov <azat.aldeshov@gmail.com>
2020-03-31 14:24:30 +0600

    Project

--- PROJECT/officeRegistrator/src/app/welcome/welcome.component.ts
+++ PROJECT/officeRegistrator/src/app/welcome/welcome.component.ts
@@ -2,8 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router'
 
 import { UserService } from '../user.service'
-import { Student } from '../oop/Student'
-import { Teacher } from '../oop/Teacher'
 import { User } from '../oop/User';
 
 @Component({
@@ -18,27 +16,6 @@ export class WelcomeComponent implements OnInit {
   constructor(private userService: UserService, private router: Router) { }
   
   ngOnInit(): void {
-    this.userService.checkCookie("userName","userPassword").subscribe(u => this.func(u));
-  }
-
-  func(u: User) {
-    if(u != null)
-    {
-      if(u.type == "Student")
-      {
-        this.u = (<Student> u);
-      }
-      else
-      {
-        if(u.type == "Teacher")
-        {
-          this.u = (<Teacher> u);
-        }
-      }
-    }
-    else
-    {
-      this.router.navigate(['']);
-    }
+    this.userService.checkCookie("userName","userPassword").subscribe(u => {if(u){this.u = u;}else{this.router.navigate(['']);}});
   }
 }
